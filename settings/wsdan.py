a10867d4d5a31f3bf0ef8277e77e418d38644225
panchenkoanton <panchenkoantonvl@gmail.com>
2020-06-08 08:22:41 +0300

    Added WSDAN settings

---
+++ settings/wsdan.py
@@ -0,0 +1,17 @@
+GPU = '0'
+learning_rate = 1e-3
+batch_size = 15
+workers = 4
+epochs = 40
+net = 'xception'
+image_size = (300, 300)
+num_attentions = 8
+beta = 5e-2
+
+tag = 'dfdc'
+pretrained = 'output/xception/best.pth'
+save_dir = './output/wsdan/'
+model_name = 'model.ckpt'
+log_name = 'train.log'
+ckpt = False
+datapath = "/mnt/ssd0/dfdc"
